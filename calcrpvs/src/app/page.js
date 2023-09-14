1dc096adff03bf06e5e01159fb7f119c7556e19c
Roberto Flávio Malheiros Barros <ba2000930@ba.trf1.gov.br>
2023-09-14 16:59:06 -0300

    LoginPJE

--- calcrpvs/src/app/page.js
+++ calcrpvs/src/app/page.js
@@ -1,7 +1,9 @@
+
+
 "use client";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-
+//import BuscaDados from "./components/BuscaDados";
 export default function Home() {
   const [form, setForm] = useState({
     total: '',
@@ -10,10 +12,31 @@ export default function Home() {
     parcelasAnteriores: 0,
     parcelasAtuais: 0,
   });
+  const [formProc, setFormProc] = useState({
+    processo: '',
+    usuario: '',
+    password: "",
+  });
 
   function handleValores(e) {
     setForm({ ...form, [e.target.name]: e.target.value });
   }
+  function handleProc(e) {
+    setFormProc({ ...formProc, [e.target.name]: e.target.value });
+  }
+
+async function handleBuscaDados(e) {
+  e.preventDefault();
+  let dados = ""
+  try {
+   // dados = await BuscaDados(formProc)
+  } catch (error) {
+    console.log("Erro ao buscar os dados!")
+  }
+ 
+
+
+}
 
   function handleCalcular(e) {
     e.preventDefault();
@@ -51,8 +74,18 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    
+    <main className="flex min-h-screen flex-rol items-center justify-between p-24">
+    
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+
+        <div className="flex-col items-center md:gap-6">
+        <p className="block text-gray-700 text-sm font-bold mb-2">
+    Cálculo Rural
+  </p>
+  <br></br>
+        <br></br>
+        <br></br>
         <form>
           <div className="grid md:grid-cols-2 md:gap-6">
             <div className="relative z-0 w-full mb-6 group">
@@ -154,6 +187,69 @@ export default function Home() {
             Calcular
           </button>
         </form>
+        <br></br>
+        <br></br>
+        <br></br>
+        
+        </div>
+        <div className="w-full max-w-xs">
+        <p className="block text-gray-700 text-sm font-bold mb-2">
+    CONSULTAR DADOS DO PROCESSO:
+  </p>
+  <br></br>
+
+
+  <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+
+    <div className="mb-4">
+
+      <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="processo">
+        Processo:
+      </label>
+      <input value={formProc.processo} 
+      onChange={handleProc}  
+      name="processo"
+      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+      id="processo" 
+      type="text" 
+      placeholder="Processo"/>
+    </div>
+    <div className="mb-4">
+      <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="usuario">
+        Usuário do PJE
+      </label>
+      <input value={formProc.usuario} 
+      onChange={handleProc}  
+      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+      id="usuario" 
+      type="text" 
+      name="usuario"
+      placeholder="Usuário"/>
+      
+    </div>
+    <div className="mb-6">
+      <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+        Senha 
+      </label>
+      <input 
+      name="password"
+      value={formProc.password} 
+      onChange={handleProc} 
+      className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
+      id="password" 
+      type="password" 
+      placeholder="Senha"/>
+
+    </div>
+    <div className="flex items-center justify-between">
+      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" onClick={handleBuscaDados}>
+        Consultar
+      </button>
+
+    </div>
+  </form>
+
+</div>
       </div>
     </main>
   );
