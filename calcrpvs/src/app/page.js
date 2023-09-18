00f6fc76a0865b54642efbd2569bfb374ee7066d
Roberto Flávio Malheiros Barros <ba2000930@ba.trf1.gov.br>
2023-09-18 17:17:25 -0300

    posição

--- calcrpvs/src/app/page.js
+++ calcrpvs/src/app/page.js
@@ -236,6 +236,24 @@ export default function Home() {
               <div className="relative z-0 w-full mb-6 group">
                 <input
                   type="number"
+                  value={form.parcelasAnteriores}
+                  onChange={handleValores}
+                  name="parcelasAnteriores"
+                  id="floating_last_name"
+                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
+                  required
+                />
+                <label
+                  htmlFor="floating_last_name"
+                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  Parcelas anteriores
+                </label>
+              </div>{" "}
+              <div className="relative z-0 w-full mb-6 group">
+                <input
+                  type="number"
                   value={form.valorAtual}
                   onChange={handleValores}
                   name="valorAtual"
@@ -255,24 +273,7 @@ export default function Home() {
                   Em construção
                 </p>
              
-              <div className="relative z-0 w-full mb-6 group">
-                <input
-                  type="number"
-                  value={form.parcelasAnteriores}
-                  onChange={handleValores}
-                  name="parcelasAnteriores"
-                  id="floating_last_name"
-                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  htmlFor="floating_last_name"
-                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  Parcelas anteriores
-                </label>
-              </div>{" "}
+              
               <div className="relative z-0 w-full mb-6 group">
                 <input
                   type="number"
