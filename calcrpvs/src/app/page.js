7791cdbaf4117139fef62c81c9fa010adc637cff
Roberto Flávio Malheiros Barros <ba2000930@ba.trf1.gov.br>
2023-09-20 09:22:22 -0300

    Herdeiro em contrução

--- calcrpvs/src/app/page.js
+++ calcrpvs/src/app/page.js
@@ -26,6 +26,8 @@ export default function Home() {
     vAutor: 0,
     jurosAutor: 0,
     jurosAdvogado: 0,
+    vHerdeiro: 0,
+    jurosHerdeiro: 0,
   });
   const [formProc, setFormProc] = useState({
     processo: "",
@@ -62,8 +64,12 @@ export default function Home() {
       juros: "",
       vAdvogado: "",
       vAutor: "",
+      qtHerdeiros: "",
+      vHerdeiro: "",
+      jurosHerdeiro: "",
       jurosAutor: "",
       jurosAdvogado: "",
+
     });
   }
   async function handleBuscaDados(e) {
@@ -279,6 +285,21 @@ export default function Home() {
                   required
                 />
               </div>
+              <div className="relative z-0 w-full mb-6 group">
+                <p className="block text-gray-700 text-sm font-bold mb-2">
+                  Qt. Herdeiros(Em Contrução):
+                </p>
+                <input
+                  type="text"
+                  value={form.qtHerdeiros}
+                  onChange={handleValores}
+                  name="qtHerdeiros"
+                  id="floating_last_name"
+                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
+                  required
+                />
+              </div>
             </div>{" "}
             <br></br>
             <p className="block text-gray-700 text-sm font-bold mb-2">
@@ -435,6 +456,48 @@ export default function Home() {
                   Juros Advogados
                 </label>
               </div>
+              <p className="block text-gray-700 text-sm font-bold mb-2">
+                Valor para Herdeiros(Em construção):
+              </p>
+              <br></br>
+              <div className="relative z-0 w-full mb-6 group">
+                <input
+                  type="number"
+                  value={form.vHerdeiro}
+                  onChange={handleValores}
+                  name="vHerdeiro"
+                  id="floating_last_name"
+                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
+                  required
+                />
+                <label
+                  htmlFor="floating_last_name"
+                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  Valor principal herdeiro
+                </label>
+              </div>
+              <div className="relative z-0 w-full mb-6 group">
+                <input
+                  type="number"
+                  value={form.jurosHerdeiro}
+                  onChange={handleValores}
+                  name="jurosHerdeiro"
+                  id="floating_last_name"
+                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
+                  required
+                />
+                <label
+                  htmlFor="floating_last_name"
+                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  Juros do herdeiro
+                </label>
+              </div>
+              
+
             </div>{" "}
             {/* <div className="relative z-0 w-full mb-6 group">
    
