926289ba3bd700e69bb5cec892f6fbed704fbd23
Roberto Flávio Malheiros Barros <ba2000930@ba.trf1.gov.br>
2023-09-21 11:47:26 -0300

    destaque

--- calcrpvs/src/app/page.js
+++ calcrpvs/src/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
-//import BuscaDados from "./components/BuscaDados";
+//import BuscaDados from "./components/BuscaPJE";
 //import Datepicker from "react-tailwindcss-datepicker";
 
 export default function Home() {
@@ -74,9 +74,11 @@ export default function Home() {
   }
   async function handleBuscaDados(e) {
     e.preventDefault();
+    console.log("Buscando dados...");
     let dados = "";
     try {
-      // dados = await BuscaDados(formProc)
+
+       
     } catch (error) {
       console.log("Erro ao buscar os dados!");
     }
@@ -182,6 +184,22 @@ export default function Home() {
           <p className="block text-gray-700 text-sm font-bold mb-2">
             CÁLCULO RPVs
           </p>
+          <div className="gap-x-10">
+              <button
+                type="submit"
+                onClick={handleCalcular}
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Calcular
+              </button>{" "}
+              <button
+                type="submit"
+                onClick={handleLimpar}
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Limpar
+              </button>
+            </div>
           <br></br>
 
           <form>
